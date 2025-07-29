628a63451fde0d6b467bb51540b04613b77ba871
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-29 18:47:15 +0300

    L-581. Create basics.ts with some primitive types examples

---
+++ basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// More complex: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 13;
+age = 14;
+
+let userName: string = 'Pegasus';
+userName = 'Doggo';
+
+let instructor: boolean = true;
+instructor = false;
+
+let hobbies: null;
+// error
+// hobbies = 'Basketball';
