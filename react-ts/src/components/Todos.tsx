383a98992be9434807892e7b931816222a0501df
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-08-01 18:06:10 +0300

    L-592. Create todo class and use it as a type for Todos items

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,9 +1,11 @@
-const Todos: React.FC<{ items: string[] }> = (props) => {
+import Todo from "../models/todo";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map(item => (
-        <li key={item}>
-          {item}
+        <li key={item.id}>
+          {item.text}
         </li>
       ))}
     </ul>
