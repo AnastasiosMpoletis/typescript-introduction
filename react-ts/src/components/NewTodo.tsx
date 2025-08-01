47b8dfb02e8c52e23d74a6236395df98da8c88ce
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-08-01 18:37:39 +0300

    L-595. Implement form text input field ref

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,12 +1,27 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // with ! we guarantee TypeScript that value is not null
+    const enteredText = todoTextInputRef.current!.value;
+    // const enteredText = todoTextInputRef.current?.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type='text' id='text' />
+      <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
